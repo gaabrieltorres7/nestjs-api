ca2ecb5e07008a5d18f01decb639bcf8d7a5a9b3
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-05 13:36:40 -0300

    feat: password hash

--- src/modules/users/use-cases/create-user.ts
+++ src/modules/users/use-cases/create-user.ts
@@ -1,4 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
 import { PrismaService } from 'src/infra/db/prisma.service';
 import { CreateUserDTO } from '../dto/user.dto';
 
@@ -17,8 +18,13 @@ export class CreateUserUseCase {
       throw new HttpException('User already exists', HttpStatus.CONFLICT);
     }
 
+    const passwordHash = await hash(data.password, 10);
+
     const newUser = await this.prisma.user.create({
-      data,
+      data: {
+        ...data,
+        password: passwordHash,
+      },
     });
 
     return newUser;

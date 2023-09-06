37c52017ad02fbb61414a6042565e0af6e32d22f
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-05 22:32:07 -0300

    feat: userRepository contract

---
+++ src/modules/users/repositories/user-repository.ts
@@ -0,0 +1,7 @@
+import { User } from '@prisma/client';
+import { UsernameAndEmail } from '../dto/user.dto';
+
+export abstract class IUserRepository {
+  abstract findByUsernameOrEmail(data: UsernameAndEmail): Promise<User | null>;
+  abstract save(data: User): Promise<User>;
+}

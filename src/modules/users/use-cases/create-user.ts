5de6a37648bc93278e0da8790e2f77e8694fc592
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-05 22:34:31 -0300

    refactor: call repository instead of prisma directly

--- src/modules/users/use-cases/create-user.ts
+++ src/modules/users/use-cases/create-user.ts
@@ -1,32 +1,27 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { hash } from 'bcrypt';
-import { PrismaService } from 'src/infra/db/prisma.service';
-import { CreateUserDTO } from '../dto/user.dto';
+import { IUserRepository } from '../repositories/user-repository';
 
 @Injectable()
 export class CreateUserUseCase {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private userRepository: IUserRepository) {}
 
-  async execute(data: CreateUserDTO) {
-    const user = await this.prisma.user.findFirst({
-      where: {
-        OR: [{ username: data.username }, { email: data.email }],
-      },
+  async execute(data: User) {
+    const user = await this.userRepository.findByUsernameOrEmail({
+      username: data.username,
+      email: data.email,
     });
 
     if (user) {
       throw new HttpException('User already exists', HttpStatus.CONFLICT);
     }
 
-    const passwordHash = await hash(data.password, 10);
+    const password = await hash(data.password, 10);
 
-    const newUser = await this.prisma.user.create({
-      data: {
-        ...data,
-        password: passwordHash,
-      },
+    return await this.userRepository.save({
+      ...data,
+      password,
     });
-
-    return newUser;
   }
 }

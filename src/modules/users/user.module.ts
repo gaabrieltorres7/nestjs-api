ae7141a9e5af7047a21ef97d9fd7e5fe4418d2fd
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-05 22:35:42 -0300

    feat: provide IUserRepository

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/infra/db/prisma.service';
+import { UserPrismaRepository } from './repositories/prisma/user-prisma-repository';
+import { IUserRepository } from './repositories/user-repository';
 import { CreateUserUseCase } from './use-cases/create-user';
 import { UserController } from './user.controller';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [CreateUserUseCase, PrismaService],
+  providers: [
+    CreateUserUseCase,
+    PrismaService,
+    {
+      provide: IUserRepository,
+      useClass: UserPrismaRepository,
+    },
+  ],
 })
 export class UserModule {}

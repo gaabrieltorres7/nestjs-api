7044f39f5b56984143e57d95a2ae3fdab68073ee
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-07 17:29:10 -0300

    feat: login module

---
+++ src/modules/login/login.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/infra/db/prisma.service';
+import { LoginController } from './login.controller';
+import { SignInUseCase } from './use-cases/sign-in';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: 'ANPnxORqspDWdSopWbOxSx',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  controllers: [LoginController],
+  providers: [PrismaService, SignInUseCase],
+})
+export class LoginModule {}

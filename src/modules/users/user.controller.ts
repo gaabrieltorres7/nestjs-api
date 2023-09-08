3eeeb18b0030f76a7ab7198104fbf1a28cabf9c0
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-08 20:36:09 -0300

    feat: route me

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,16 +1,35 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
+import { AuthGuard } from 'src/infra/providers/auth-guard.provider';
 import { CreateUserValidationPipe } from './pipe/create-user.validation.pipe';
 import { CreateUserUseCase } from './use-cases/create-user';
+import { ProfileUserUseCase } from './use-cases/profile-user';
 
 @Controller('/users')
 export class UserController {
-  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+    private readonly profileUserUseCase: ProfileUserUseCase,
+  ) {}
 
   @Post()
   @UsePipes(CreateUserValidationPipe)
   async create(@Body() data: User) {
     const newUser = await this.createUserUseCase.execute(data);
     return newUser;
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard)
+  async me(@Request() { user }) {
+    return this.profileUserUseCase.execute(user.sub);
   }
 }

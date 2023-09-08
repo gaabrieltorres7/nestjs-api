650967e8573790320f40ac11f5e50d64370d6076
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-08 20:55:40 -0300

    feat: userCreatedDTO

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -5,6 +5,11 @@ export type CreateUserDTO = {
   name: string;
 };
 
+export type UserCreatedDTO = {
+  id: string;
+  createdAt: Date;
+} & CreateUserDTO;
+
 export type UsernameAndEmail = {
   username: string;
   email: string;

2476da1b5afa4b57c8fef3fb84278521391a5e08
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-08 21:59:53 -0300

    feat: TaskUserPrismaRepository

---
+++ src/modules/tasks/repositories/prisma/task-user-prisma-repository.ts
@@ -0,0 +1,32 @@
+import { PrismaService } from 'src/infra/db/prisma.service';
+import {
+  TaskUserRequestDTO,
+  TaskUserResponseDTO,
+} from '../../dto/task-user.dto';
+import { ITaskRepository } from '../task-user-repository';
+
+export class TaskUserPrismaRepository implements ITaskRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async save(data: TaskUserRequestDTO): Promise<TaskUserResponseDTO> {
+    return this.prisma.taskUser.create({
+      data: {
+        task: {
+          create: {
+            description: data.description,
+            endAt: data.endAt,
+            startAt: data.startAt,
+            title: data.title,
+            priority: data.priority,
+            status: data.status,
+          },
+        },
+        user: {
+          connect: {
+            id: data.userId,
+          },
+        },
+      },
+    });
+  }
+}

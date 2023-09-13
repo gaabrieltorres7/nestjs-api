d048a3a56ea69ee32162e9fbffff545d1103a9b4
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-12 22:38:23 -0300

    feat: uploadAvatar

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -3,19 +3,26 @@ import {
   Controller,
   Get,
   Post,
+  Put,
   Request,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthGuard } from 'src/infra/providers/auth-guard.provider';
+import { FileDTO } from './dto/user.dto';
 import { CreateUserSchemaDTO } from './schemas/create-user-schema';
 import { CreateUserUseCase } from './use-cases/create-user';
 import { ProfileUserUseCase } from './use-cases/profile-user';
+import { UploadAvatarUserUseCase } from './use-cases/upload-avatar-user';
 
 @Controller('/users')
 export class UserController {
   constructor(
     private readonly createUserUseCase: CreateUserUseCase,
     private readonly profileUserUseCase: ProfileUserUseCase,
+    private readonly uploadAvatarUserUseCase: UploadAvatarUserUseCase,
   ) {}
 
   @Post()
@@ -33,5 +40,17 @@ export class UserController {
   @UseGuards(AuthGuard)
   async me(@Request() { user }) {
     return this.profileUserUseCase.execute(user.sub);
+  }
+
+  @Put('/avatar')
+  @UseInterceptors(FileInterceptor('file'))
+  @UseGuards(AuthGuard)
+  async uploadAvatar(@Request() req, @UploadedFile() file: FileDTO) {
+    const result = await this.uploadAvatarUserUseCase.execute({
+      userId: req.user.sub,
+      file,
+    });
+
+    return result;
   }
 }
--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/infra/db/prisma.service';
+import { IStorage } from 'src/infra/providers/storage/storage';
+import { SupabaseStorage } from 'src/infra/providers/storage/supabase-storage';
 import { UserPrismaRepository } from './repositories/prisma/user-prisma-repository';
 import { IUserRepository } from './repositories/user-repository';
 import { CreateUserUseCase } from './use-cases/create-user';
 import { ProfileUserUseCase } from './use-cases/profile-user';
+import { UploadAvatarUserUseCase } from './use-cases/upload-avatar-user';
 import { UserController } from './user.controller';
 
 @Module({
@@ -12,10 +15,15 @@ import { UserController } from './user.controller';
   providers: [
     CreateUserUseCase,
     ProfileUserUseCase,
+    UploadAvatarUserUseCase,
     PrismaService,
     {
       provide: IUserRepository,
       useClass: UserPrismaRepository,
+    },
+    {
+      provide: IStorage,
+      useClass: SupabaseStorage,
     },
   ],
 })

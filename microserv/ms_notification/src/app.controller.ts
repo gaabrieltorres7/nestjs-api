b3e471fd48dc5d2a8982a4e2fd7ff307d1506ef8
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-15 20:57:13 -0300

    feat: eventPattern taskNotification

--- microserv/ms_notification/src/app.controller.ts
+++ microserv/ms_notification/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('task_notification')
+  taskNotification(data: any) {
+    console.log('Recebendo notificação de tarefa');
+    console.log(data);
   }
 }

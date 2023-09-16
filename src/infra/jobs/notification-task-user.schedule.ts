70728abe0d77e98993b85f96a3d1b5e91744bef2
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-16 16:57:32 -0300

    refactor: inject notification on NotificationTaskUserSchedule

--- src/infra/jobs/notification-task-user.schedule.ts
+++ src/infra/jobs/notification-task-user.schedule.ts
@@ -1,14 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { ITaskRepository } from 'src/modules/tasks/repositories/task-user-repository';
 
+type MessageDTO = {
+  email: string;
+  startAt: Date;
+  endAt: Date;
+  name: string;
+  title: string;
+  description: string;
+};
+
 @Injectable()
 export class NotificationTaskUserSchedule {
-  constructor(private taskRepository: ITaskRepository) {}
+  constructor(
+    private taskRepository: ITaskRepository,
+    @Inject('NOTIFICATION') private readonly notificationClient: ClientProxy,
+  ) {} //using Inject to get the ms client
 
   @Cron(CronExpression.EVERY_5_SECONDS)
   async getAllTasksDay() {
     const allTasks = await this.taskRepository.findAllStartDay();
-    console.log(allTasks);
+
+    if (allTasks) {
+      allTasks.forEach((task) => {
+        const message: MessageDTO = {
+          email: task.user.email,
+          startAt: task.task.startAt,
+          endAt: task.task.endAt,
+          name: task.user.name,
+          title: task.task.title,
+          description: task.task.description,
+        };
+        this.notificationClient.emit('task_notification', message);
+      });
+    }
   }
 }

eb04843559dcd0bc3130ef5b3377461bc53dcc73
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-17 16:22:10 -0300

    feat: MailerModule

--- microserv/ms_notification/src/app.module.ts
+++ microserv/ms_notification/src/app.module.ts
@@ -1,9 +1,22 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'stephon.strosin17@ethereal.email',
+          pass: 'gymSJmdC5CjRCF5KZt',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

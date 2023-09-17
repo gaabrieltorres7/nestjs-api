d5c7ecfa1b9c5b6191a6fd613fcdd3be4c27643f
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-17 16:23:02 -0300

    feat: sendMail for task notification

--- microserv/ms_notification/src/app.controller.ts
+++ microserv/ms_notification/src/app.controller.ts
@@ -1,14 +1,30 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
-import { AppService } from './app.service';
+
+type NotificationDTO = {
+  email: string;
+  startAt: Date;
+  endAt: Date;
+  name: string;
+  title: string;
+  description: string;
+};
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private mailerService: MailerService) {}
 
   @EventPattern('task_notification')
-  taskNotification(data: any) {
+  async taskNotification(data: NotificationDTO) {
     console.log('Recebendo notificação de tarefa');
     console.log(data);
+    await this.mailerService.sendMail({
+      to: data.email,
+      subject: 'Nova tarefa',
+      from: 'taskmanager@gmail.com',
+      text: `Olá, ${data.name}! Você tem uma nova tarefa: ${data.title} 
+      que começa em ${data.startAt} e termina em ${data.endAt}.`,
+    });
   }
 }

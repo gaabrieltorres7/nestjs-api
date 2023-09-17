7c29915ee98de59e0aaca514febb336543df6a01
Gabriel Torres <gaabrieltt7@gmail.com>
2023-09-17 15:53:31 -0300

    refactor: remove useless function

--- microserv/ms_notification/src/app.controller.ts
+++ microserv/ms_notification/src/app.controller.ts
@@ -1,15 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
   @EventPattern('task_notification')
   taskNotification(data: any) {
